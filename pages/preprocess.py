1c202d7c16944c5a96e7bc5bf9f4e9ef93cc9a0f
RacimRgh <racim.rgh@gmail.com>
2022-03-23 21:34:52 +0100

    fix preprocess

--- pages/preprocess.py
+++ pages/preprocess.py
@@ -37,20 +37,34 @@ def app():
 
         ctype = col2.selectbox("Select Column Type",
                                options=column_options, index=current_index)
-
-        st.write("""Select your column name and the new type from the data.
+        e1, e2 = st.columns(2)
+        e1.write("""Select your column name and the new type from the data.
                     To submit all the changes, click on *Submit changes* """)
+        chg_type = e2.button("Change Column Type")
+
+        a1, a2, a3, a4, a5 = st.columns(5)
+        nas = a1.checkbox("Delete NA's", key=current_index)
+        dum = a2.checkbox("Dumify", key=current_index)
+        cst = a3.checkbox("Delete 3efsa", key=current_index)
+        if a5.button("Submit"):
+            print(nas, dum, cst)
+            if nas:
+                df.dropna(subset=[name])
+                nas = False
+            if dum:
+                df = pd.get_dummies(df, columns=[name])
+                dum = False
+            if cst:
+                pass
 
         b1, b2, b3, b4, b5 = st.columns(5)
-        chg_type = b1.button("Change Column Type")
-        dum = b2.button("Dummify")
-        de = b3.button("Delete")
+        apply = b3.button("Revert last change")
         rev = b4.button("Revert all changes")
-        apply = b5.button("Apply all changes")
-        st.write(
-            '<style>div.row-widget.stRadio > div{flex-direction:row;justify-content: center;} </style>', unsafe_allow_html=True)
-        choose = st.radio("Pre treatement", ("Delete NA values",
-                          "Delete constant columns", "Dummify"))
+        de = b5.button("Delete")
+        # st.write(
+        #     '<style>div.row-widget.stRadio > div{flex-direction:row;justify-content: center;} </style>', unsafe_allow_html=True)
+        # choose = st.radio("Pre treatement", ("Delete NA values",
+        #                   "Delete constant columns", "Dummify"))
         if chg_type:
             if ctype == 'numerical':
                 df[name] = pd.to_numeric(df[name], errors='ignore')
@@ -58,8 +72,6 @@ def app():
                 df = df.astype({name: str}, errors='ignore')
             elif ctype == 'categorical':
                 df = df.astype({name: 'category'}, errors='ignore')
-        if dum:
-            df = pd.get_dummies(df, columns=[name])
         if de:
             df.drop(name, inplace=True, axis=1)
         if rev:
@@ -68,15 +80,20 @@ def app():
             df = df
 
         st.write("Your changes have been made!")
-        c1, c2, c3, c4, c5 = st.columns(5)
-        c1.write('Column')
-        c2.write('Type')
-        c3.write('Dummiffy')
-        c4.write('Delete')
-        c5.write('Type modification')
+        c = st.columns(6)
+        c[0].write('Column')
+        c[1].write('Type')
+        c[2].write('Column')
+        c[3].write('Type')
+        c[4].write('Column')
+        c[5].write('Type')
+        i = 0
         for idx, val in enumerate(df.columns):
-            c1.write(val)
-            c2.write(df[val].dtype)
+            if i == 6:
+                i = 0
+            c[i].write(val)
+            c[i+1].write(df[val].dtype)
+            i += 2
 
             # c3.checkbox("Dumify", key=idx)
             # c4.checkbox("Delete", key=idx)
