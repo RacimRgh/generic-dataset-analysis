7ae9e07806c7b1326d9b21c9d6e74606b4e276b4
RacimRgh <racim.rgh@gmail.com>
2022-03-28 01:31:58 +0200

    commented code

--- utils/visualisation.py
+++ utils/visualisation.py
@@ -27,19 +27,19 @@ class ClassificationPlot:
         cm_display = ConfusionMatrixDisplay(
             confusion_matrix=cm, display_labels=classes)
         fig = cm_display.plot(include_values=True, xticks_rotation='vertical')
-        return fig, ax
+        return fig
 
 
-def plot_metrics(x_test, y_test):
+# def plot_metrics(x_test, y_test):
 
-    st.subheader("Confusion Matrix")
-    plot_confusion_matrix(model, x_test, y_test, display_labels=class_names)
-    st.pyplot()
+#     st.subheader("Confusion Matrix")
+#     plot_confusion_matrix(model, x_test, y_test, display_labels=class_names)
+#     st.pyplot()
 
-    st.subheader("ROC Curve")
-    plot_roc_curve(model, x_test, y_test)
-    st.pyplot()
+#     st.subheader("ROC Curve")
+#     plot_roc_curve(model, x_test, y_test)
+#     st.pyplot()
 
-    st.subheader("Precision-Recall Curve")
-    plot_precision_recall_curve(model, x_test, y_test)
-    st.pyplot()
+#     st.subheader("Precision-Recall Curve")
+#     plot_precision_recall_curve(model, x_test, y_test)
+#     st.pyplot()
