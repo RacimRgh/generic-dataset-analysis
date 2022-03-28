d3c95574385f6bb07a5449655091cc16ace79d46
RacimRgh <racim.rgh@gmail.com>
2022-03-28 17:06:58 +0200

    added error for pca

--- pages/pca.py
+++ pages/pca.py
@@ -14,34 +14,41 @@ def app():
         df = pd.read_csv('data.csv')
         df_visual = df.copy()
 
-        categorical = df_visual.select_dtypes(
-            include=['object']).columns.values
-        numerical = df_visual.select_dtypes(include=[np.number]).columns.values
+        st.title("Dimension reduction")
 
-    cats = st.selectbox("Select categorical columns ", categorical)
-    st.markdown("#### Variance expliquee ")
-    var = st.slider("Percentage explained variance",
-                    min_value=0.4,
-                    max_value=0.95,
-                    step=0.1,
-                    value=0.8,
-                    help="This is the value which will be used in pca for dimension reduction. Default = 80%")
+        if pd.isna(df).any().any():
+            st.error(
+                "You have NAN values in your dataframe, please go to preprocessing before using PCA")
+        else:
+            categorical = df_visual.select_dtypes(
+                include=['object']).columns.values
+            numerical = df_visual.select_dtypes(
+                include=[np.number]).columns.values
 
-    df2 = df_visual[numerical]
-    df2 = StandardScaler().fit_transform(df2)
+            cats = st.selectbox("Select categorical columns ", categorical)
+            st.markdown("#### Variance expliquee ")
+            var = st.slider("Percentage explained variance",
+                            min_value=0.4,
+                            max_value=0.95,
+                            step=0.1,
+                            value=0.8,
+                            help="This is the value which will be used in pca for dimension reduction. Default = 80%")
 
-    pca = PCA(n_components=var)
-    pca.fit(df2)
-    df_p = pca.fit_transform(df2)
-    if cats:
-        st.title(
-            'Presentation de la reduction en deux dimensions(2 premiers composants principaux)')
-        fig = px.scatter(df_p, x=0, y=1, color=df_visual[cats])
-        st.write(fig)
-        st.title(
-            'Presentation de la reduction en trois dimensions(3 premiers composants principaux)')
-        fig2 = px.scatter_3d(
-            df_p, x=0, y=1, z=2,
-            labels={'0': ' PCA1', '1': 'PCA2', '2': 'PCA3'},
-            color=df_visual[cats])
-        st.write(fig2)
+            df2 = df_visual[numerical]
+            df2 = StandardScaler().fit_transform(df2)
+
+            pca = PCA(n_components=var)
+            pca.fit(df2)
+            df_p = pca.fit_transform(df2)
+            if cats:
+                st.title(
+                    "Visualisation of the dimension reduction in 2D (2 principal components)")
+                fig = px.scatter(df_p, x=0, y=1, color=df_visual[cats])
+                st.write(fig)
+                st.title(
+                    "Visualisation of the dimension reduction in 3D (3 principal components)")
+                fig2 = px.scatter_3d(
+                    df_p, x=0, y=1, z=2,
+                    labels={'0': ' PCA1', '1': 'PCA2', '2': 'PCA3'},
+                    color=df_visual[cats])
+                st.write(fig2)
